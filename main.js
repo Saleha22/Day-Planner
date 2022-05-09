0df400c44aadecacf9be9c96e8a1fd0f81cec072
Saleha22 <102179415+Saleha22@users.noreply.github.com>
2022-05-09 20:03:12 +0100

    added popup time

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
   // on page load
   $("#currentDay").html(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+  hourlyBlock();
 });
+
+const hourlyBlock = () => {
+  alert("loadHourlyblock");
+};
