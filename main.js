a7006436cce8806b2df9c91faf517847de1891b1
Saleha22 <102179415+Saleha22@users.noreply.github.com>
2022-05-10 12:21:28 +0100

    added time and save button not working properly though need to fix

--- main.js
+++ main.js
@@ -7,3 +7,44 @@ $(document).ready(function () {
 const hourlyBlock = () => {
   alert("loadHourlyblock");
 };
+
+const renderTimeBlock = (workingHour) => {
+  console.log(workingHour);
+};
+workingHours.forEach(renderTimeBlock);
+
+const getClassName = (workingHour) => {
+  const currentHour = moment().hour();
+  //if workingHour is present
+  if (workingHour === currentHour) {
+    return "present";
+  }
+  //if workingHour is future
+  if (workingHour > currentHour) {
+    return "future";
+  }
+  return "past";
+  //else past
+};
+
+const readFromLocalStorage = (key, defaultValue) => {
+  // get from LS using key name
+  const dataFromLS = localStorage.getItem(key);
+
+  // parse data from LS
+  const parsedData = JSON.parse(dataFromLS);
+
+  if (parsedData) {
+    return parsedData;
+  } else {
+    return defaultValue;
+  }
+};
+
+const writeToLocalStorage = (key, value) => {
+  // convert value to string
+  const stringifiedValue = JSON.stringify(value);
+
+  // set stringified value to LS for key name
+  localStorage.setItem(key, stringifiedValue);
+};
